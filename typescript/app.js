90b0546c8253b96efd0449559a43539e0d92acef
UMME KASHAF <ummekashaf19@gmail.com>
2025-06-27 12:58:49 +0500

    Type Annotations

---
+++ typescript/app.js
@@ -0,0 +1,6 @@
+//Type Annotations
+var language = "kashaf";
+// language =212________ error because type is string
+var numbers = [2, 4, 6, 8];
+var cities = ["Karachi, Islamabad, Lahore,"];
+var dates = 12;
---
+++ typescript/app.ts
@@ -0,0 +1,10 @@
+//Type Annotations
+
+let language:string = "kashaf"
+// language =212________ error because type is string
+
+const numbers:number[] =[2, 4, 6, 8]
+
+let cities:string[] = ["Karachi, Islamabad, Lahore,"]
+
+let dates:number = 12
